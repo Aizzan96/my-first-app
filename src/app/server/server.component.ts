43e3b9627164e7798ead0d910595d446ec66bba1
Aizzan <aizzan.mistoh@abssasia.com>
2024-03-21 14:21:44 +0800

    base angular

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+
+import { Component } from '@angular/core';
+
+//decorator enhance class
+@Component ({
+
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+})
+
+//class 
+export class  ServerComponent {
+
+
+}
